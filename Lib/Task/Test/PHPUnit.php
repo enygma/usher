fdc7730f7bdc78f6742a8637f7d5c4ac0a831269
Chris Cornutt <enygma@phpdeveloper.org>
2011-12-13 21:37:04 +0000

    adding placeholders for testing tasks for phpunit/simpletest

---
+++ Lib/Task/Test/PHPUnit.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Lib\Task\Test;
+
+/**
+ * PHPUnit interface
+ *
+ * @package Usher
+ */
+class PHPUnit extends \Lib\Task
+{
+        public function execute()
+        {
+		// TODO
+        }
+}
+
