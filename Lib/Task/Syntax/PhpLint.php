19d7eb158343fc5867035a5a2b626da2c0aea853
Chris Cornutt <enygma@phpdeveloper.org>
2011-12-16 13:53:16 +0000

    adding a sample php lint task

--- Lib/Task/Syntax/PhpLint.php
+++ Lib/Task/Syntax/PhpLint.php
@@ -2,16 +2,28 @@
 
 namespace Lib\Task\Syntax;
 
+/**
+ * Runs a php lint check on the project directory
+ * 
+ * @package usher
+ */
 class PhpLint extends \Lib\Task
 {
 	
 	public function execute()
 	{
-		
+		// get the project basedir
+		$projectDir = $this->getProjectConfig('projectBase');
+
+		if($projectDir != null){
+			$exec = 'php -l '.$projectDir;
+			Console\Execute::run($exec);
+		}else{
+			throw new \Exception('Could not determine project directory (projectBase)');
+		}
 		
 	}
 
 }
-
 
 ?>
