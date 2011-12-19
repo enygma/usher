aa46136010f92854b230a8eb7b09f70e0c9e6b76
Chris Cornutt <enygma@phpdeveloper.org>
2011-12-18 21:36:15 -0600

    making the git pull task work

--- Lib/Task/Vcs/Git/Pull.php
+++ Lib/Task/Vcs/Git/Pull.php
@@ -2,13 +2,28 @@
 
 namespace Usher\Lib\Task\Vcs\Git;
 
+/**
+ * Task executing a "pull" request on a git repository
+ */
 class PullTask extends \Usher\Lib\Task
 {
+    /**
+     * Default "git pull" command
+     */
+    private $_command = 'git pull';
     
     public function execute()
     {
-        $this->getOption('repositoryPath');
+        $destinationPath = $this->getOption('destinationPath'); 
+        $currentDir      = getcwd();
         
+        // change to the destination directory
+        chdir($destinationPath);
+
+        \Usher\Lib\Console\Execute::run($this->_command);
+
+        // change back to our working directory
+        chdir($currentDir);
     }
 
 }
