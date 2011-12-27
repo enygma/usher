7dc10db620b9e58529c82c6cdaea2778aba14a44
Chris Cornutt <enygma@phpdeveloper.org>
2011-12-27 15:06:09 +0000

    adding custom task directory command line option, just sets flag for right now

---
+++ Lib/Console/Option/TaskDir.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Definiton of a custom tasks directory
+ *   Allows a source for additional tasks to be autloaded (single dir level)
+ *
+ * PHP Version 5
+ *
+ * @category Build
+ * @package  User
+ * @author   Chris Cornutt <ccornutt@phpdeveloper.org>
+ * @license  http://www.opensource.org/licenses/mit-license.php MIT
+ * @link     http://github.com/enygma/usher
+ */
+
+namespace Usher\Lib\Console\Option;
+
+/**
+ * Class Quiet
+ *
+ * @category TaskDir
+ * @package  User
+ * @author   Chris Cornutt <ccornutt@phpdeveloper.org>
+ * @license  http://www.opensource.org/licenses/mit-license.php MIT
+ * @link     http://github.com/enygma/usher
+ * @description Help option (display this info)
+ */
+class TaskDir extends \Usher\Lib\Console\Option
+{
+    /**
+     * Execute the TaskDir option
+     *
+     * @param array $argumentData Argument data
+     *
+     * @return bool Stop/Don't stop execution
+     */
+    public function execute($argumentData)
+    {
+        $directory = $argumentData[1];
+        if (!is_dir($directory)) {
+            throw new \Exception(
+                'Tasks directory "'.$directory.'" could not be accessed.'
+            );
+        }
+        \Usher\Lib\Console::setOption('customTaskDir', $directory);
+        return true;
+    }
+}
+
+?>
